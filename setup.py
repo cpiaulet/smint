0dbdbd5e5f8ff122ad3f78a18f884b32b93f9f29
cpiaulet <caroline.piaulet@umontreal.ca>
2020-10-14 11:01:26 -0400

    update package description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("smint/version.py", "r") as f:
 
 setup(name='smint',
       version=__version__,
-      description='Inferring envelope mass fractions using the Lopez&Fortney model grids',
+      description='Inferring HHe and H2O mass fractions using the Lopez&Fortney 2014 and Zeng 2016 model grids',
       url='http://github.com/cpiaulet/smint',
       author='Caroline Piaulet',
       author_email='caroline.piaulet@umontreal.ca',
