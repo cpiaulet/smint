b0de26a0b84b4bd7e6f95c067239a4e8c855d33a
Caroline Piaulet-Ghorayeb <38121553+cpiaulet@users.noreply.github.com>
2026-08-11 08:47:09 -0500

    import at top of file

--- smint/fit_irrowev.py
+++ smint/fit_irrowev.py
@@ -6,6 +6,7 @@ import corner
 import matplotlib.pyplot as plt
 from astropy.io import ascii
 import os
+from datetime import datetime  # Get current date and time
 
 
 # %% utilities for interpolation
@@ -201,7 +202,6 @@ def ini_fit(params, grid_lim=None):
     if None, uses the bounds from the Aguichine et al. (2025) grid
     output: initial positions of the walkers and labels for the fitted para
     """
-    from datetime import datetime  # Get current date and time
     now = datetime.now()  # Format as YYYYMMDD_HHhMMmSSs
     Datestr = now.strftime("%Y%m%d_%Hh%Mm%Ss")
     print(Datestr)  # Output: 20260715_170942s (based on current time)
